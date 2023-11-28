66bd1021e64c5a6898627570257ee0b162c9c275
Michael <yang335@cs.ucsb.edu>
2023-11-28 14:55:57 -0800

    init

---
+++ build/CMakeFiles/Game32.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game32.dir/src/main.cpp.o"
+  "CMakeFiles/Game32.dir/src/main.cpp.o.d"
+  "Game32"
+  "Game32.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game32.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/OpenGLProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLProject.dir/Dependencies/Glad/glad.c.o"
+  "CMakeFiles/OpenGLProject.dir/Dependencies/Glad/glad.c.o.d"
+  "CMakeFiles/OpenGLProject.dir/main.cpp.o"
+  "CMakeFiles/OpenGLProject.dir/main.cpp.o.d"
+  "OpenGLProject"
+  "OpenGLProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
