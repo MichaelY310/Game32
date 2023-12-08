536ce635ac0071fe31ac8b2a89a610fd019a2d6b
Michael <yang335@cs.ucsb.edu>
2023-12-08 13:03:56 -0800

    debug

---
+++ build/CMakeFiles/Game32.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/cs/student/yang335/Desktop/Game32/src/Core/Input.cpp" "CMakeFiles/Game32.dir/src/Core/Input.cpp.o" "gcc" "CMakeFiles/Game32.dir/src/Core/Input.cpp.o.d"
+  "/cs/student/yang335/Desktop/Game32/src/Render/DataTypes.cpp" "CMakeFiles/Game32.dir/src/Render/DataTypes.cpp.o" "gcc" "CMakeFiles/Game32.dir/src/Render/DataTypes.cpp.o.d"
+  "/cs/student/yang335/Desktop/Game32/src/Render/Renderer.cpp" "CMakeFiles/Game32.dir/src/Render/Renderer.cpp.o" "gcc" "CMakeFiles/Game32.dir/src/Render/Renderer.cpp.o.d"
+  "/cs/student/yang335/Desktop/Game32/src/Render/Texture.cpp" "CMakeFiles/Game32.dir/src/Render/Texture.cpp.o" "gcc" "CMakeFiles/Game32.dir/src/Render/Texture.cpp.o.d"
+  "/cs/student/yang335/Desktop/Game32/src/Scene/BossHPPanel.cpp" "CMakeFiles/Game32.dir/src/Scene/BossHPPanel.cpp.o" "gcc" "CMakeFiles/Game32.dir/src/Scene/BossHPPanel.cpp.o.d"
+  "/cs/student/yang335/Desktop/Game32/src/Scene/Entity.cpp" "CMakeFiles/Game32.dir/src/Scene/Entity.cpp.o" "gcc" "CMakeFiles/Game32.dir/src/Scene/Entity.cpp.o.d"
+  "/cs/student/yang335/Desktop/Game32/src/Scene/Scene.cpp" "CMakeFiles/Game32.dir/src/Scene/Scene.cpp.o" "gcc" "CMakeFiles/Game32.dir/src/Scene/Scene.cpp.o.d"
+  "/cs/student/yang335/Desktop/Game32/src/main.cpp" "CMakeFiles/Game32.dir/src/main.cpp.o" "gcc" "CMakeFiles/Game32.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Game32.dir/src/Render/DataTypes.cpp.o.d
@@ -0,0 +1,69 @@
+CMakeFiles/Game32.dir/src/Render/DataTypes.cpp.o: \
+ /cs/student/yang335/Desktop/Game32/src/Render/DataTypes.cpp \
+ /usr/include/stdc-predef.h \
+ /cs/student/yang335/Desktop/Game32/src/Render/DataTypes.h \
+ /usr/include/c++/13/cmath /usr/include/c++/13/bits/requires_hosted.h \
+ /usr/include/c++/13/x86_64-redhat-linux/bits/c++config.h \
+ /usr/include/bits/wordsize.h \
+ /usr/include/c++/13/x86_64-redhat-linux/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/timesize.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/13/x86_64-redhat-linux/bits/cpu_defines.h \
+ /usr/include/c++/13/pstl/pstl_config.h \
+ /usr/include/c++/13/bits/cpp_type_traits.h \
+ /usr/include/c++/13/ext/type_traits.h /usr/include/math.h \
+ /usr/include/bits/libc-header-start.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
+ /usr/include/bits/iscanonical.h /usr/include/c++/13/bits/std_abs.h \
+ /usr/include/stdlib.h \
+ /usr/lib/gcc/x86_64-redhat-linux/13/include/stddef.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/sys/types.h /usr/include/bits/types/clock_t.h \
+ /usr/include/bits/types/clockid_t.h /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/timer_t.h /usr/include/bits/stdint-intn.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/endianness.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h \
+ /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h /usr/include/c++/13/bits/specfun.h \
+ /usr/include/c++/13/bits/stl_algobase.h \
+ /usr/include/c++/13/bits/functexcept.h \
+ /usr/include/c++/13/bits/exception_defines.h \
+ /usr/include/c++/13/ext/numeric_traits.h \
+ /usr/include/c++/13/bits/stl_pair.h /usr/include/c++/13/type_traits \
+ /usr/include/c++/13/bits/move.h /usr/include/c++/13/bits/utility.h \
+ /usr/include/c++/13/bits/stl_iterator_base_types.h \
+ /usr/include/c++/13/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/13/bits/concept_check.h \
+ /usr/include/c++/13/debug/assertions.h \
+ /usr/include/c++/13/bits/stl_iterator.h \
+ /usr/include/c++/13/bits/ptr_traits.h /usr/include/c++/13/debug/debug.h \
+ /usr/include/c++/13/bits/predefined_ops.h /usr/include/c++/13/bit \
+ /usr/include/c++/13/limits /usr/include/c++/13/tr1/gamma.tcc \
+ /usr/include/c++/13/tr1/special_function_util.h \
+ /usr/include/c++/13/tr1/bessel_function.tcc \
+ /usr/include/c++/13/tr1/beta_function.tcc \
+ /usr/include/c++/13/tr1/ell_integral.tcc \
+ /usr/include/c++/13/tr1/exp_integral.tcc \
+ /usr/include/c++/13/tr1/hypergeometric.tcc \
+ /usr/include/c++/13/tr1/legendre_function.tcc \
+ /usr/include/c++/13/tr1/modified_bessel_func.tcc \
+ /usr/include/c++/13/tr1/poly_hermite.tcc \
+ /usr/include/c++/13/tr1/poly_laguerre.tcc \
+ /usr/include/c++/13/tr1/riemann_zeta.tcc
